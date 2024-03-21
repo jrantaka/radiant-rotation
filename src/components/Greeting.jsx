dcd959dbb46d4f49e5cdc987625e57444aa3d5a9
Juho Rantakari <juho@rantakari.com>
2024-03-21 19:41:27 +0200

    Tailwind

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,12 @@ export default function Greeting({ messages }) {
 	return (
 		<div>
 			<h3>{greeting}! Thank you for visiting!</h3>
-			<button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+			<button
+				onClick={() => setGreeting(randomMessage())}
+				class="border-[1px] p-2 rounded-lg hover:bg-stone-500 active:bg-stone-400"
+			>
+				New Greeting
+			</button>
 		</div>
 	);
 }
