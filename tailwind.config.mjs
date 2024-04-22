e84d1991825daeb5412f5e0deeaeef08806ed06e
Juho Rantakari <juho@rantakari.com>
2024-04-22 09:30:20 +0300

    Adding images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,15 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			serif: ["Spectral", "ui-sans-serif", "system-ui", "-apple-system"],
-			sans: ["Inter", "ui-sans-serif", "system-ui", "-apple-system"],
-			headline: ["InterDisplay", "ui-sans-serif", "system-ui", "-apple-system"],
+			serif: ["Plantin MT Pro", "ui-sans-serif", "system-ui", "-apple-system"],
+			sans: ["Nitti Grotesk", "ui-sans-serif", "system-ui", "-apple-system"],
+			headline: [
+				"Neue Haas Text Pro",
+				"InterDisplay",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+			],
 		},
 		extend: {},
 	},
