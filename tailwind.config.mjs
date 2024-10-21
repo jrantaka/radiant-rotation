03e85d638ec18c1205c22716a2504158fee9d88a
Juho Rantakari <juho@rantakari.com>
2024-10-21 19:47:37 +0300

    Changing typefaces

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,10 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			serif: ["linotype-sabon", "ui-sans-serif", "system-ui", "-apple-system"],
-			sans: ["Neue Haas Grotesk Text Pro", "system-ui", "-apple-system"],
+			serif: ["IBM Plex Serif", "ui-sans-serif", "system-ui", "-apple-system"],
+			sans: ["IBM Plex Sans", "system-ui", "-apple-system"],
 			headline: [
-				"neue-haas-grotesk-text",
+				"IBM Plex Sans Condensed",
 				"ui-sans-serif",
 				"system-ui",
 				"-apple-system",
